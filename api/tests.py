92dbc96712b518bba316ba912d4616a3f3e22f05
g-libardi <contato@libardi.dev.br>
2024-10-31 15:16:14 -0300

    test: test ping route response

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestPingView(TestCase):
+    def test_ping_view_(self):
+        """
+        Expects a 200 response with a {'message': 'pong'} JSON response
+        """
+        response = self.client.get(reverse('ping'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'message': 'pong'})
