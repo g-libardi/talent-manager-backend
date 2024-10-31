69739ee30ed830586e31741e6afd43c998ca65b3
g-libardi <contato@libardi.dev.br>
2024-10-31 14:51:47 -0300

    docs: add api/ping metadata

--- api/views.py
+++ api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView, Response, Request
 from rest_framework import status
+from drf_spectacular.utils import extend_schema
 
 # Create your views here.
 class PingView(APIView):
-    '''
-    Health check endpoint
-    '''
+    @extend_schema(
+        summary='Ping endpoint',
+        description='Use this endpoint to check if the API is running',
+        responses={200: {'message': 'pong'}},
+    )
     def get(self, _: Request):
         return Response({'message': 'pong'}, status=status.HTTP_200_OK)
