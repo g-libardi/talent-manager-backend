b281daaf4ce0511dedb5b824553305a70ae0da2a
g-libardi <contato@libardi.dev.br>
2024-11-03 20:39:38 -0300

    feat: add candidate model

--- candidates/models.py
+++ candidates/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, TextChoices, DateField
 
-# Create your models here.
+
+class Status(TextChoices):
+    APPLIED = 'APPLIED', 'Applied'
+    INVITED = 'INVITED', 'Invited'
+    REJECTED = 'REJECTED', 'Rejected'
+
+
+class Candidate(Model):
+    first_name = CharField(max_length=255)
+    last_name = CharField(max_length=255)
+    birth_date = DateField()
+    skills = CharField(max_length=1024)
+    status = CharField(choices=Status.choices, default=Status.APPLIED, max_length=24)
